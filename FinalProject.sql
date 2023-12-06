7f8fc62e5edd0e352d54d4768db3562745204dac
jkoenigseder <jkoenigseder2@cub.uca.edu>
2023-12-05 19:12:52 -0600

    Update FinalProject.sql
    Added 2 tv shows & act/write/direct for each

--- FinalProject.sql
+++ FinalProject.sql
@@ -254,10 +254,16 @@ VALUES('tt4154796', 'Avengers: Endgame', 'PG-13', '3h 1m', '2019', 'fter the dev
 
 
 --INSERT TV_Shows (Id, Title, Age_rating, Seasons, Year, Storyline)
-
+INSERT INTO TV_show(Id, Title, Age_rating, Seasons, Year, Storyline)
+VALUES('tt9140554', 'Loki','TV-14', 2, 'The mercurial villain Loki resumes his role as the God of Mischief in a new series that takes place after the events of “Avengers: Endgame.”'
 
 
 --Insert
+INSERT INTO TV_show(Id, Title, Age_rating, Seasons, Year, Storyline)
+VALUES('tt9140554', 'Loki','TV-14', 2, 2021, 'The mercurial villain Loki resumes his role as the God of Mischief in a new series that takes place after the events of “Avengers: Endgame.”')
+
+INSERT INTO TV_show
+VALUES('tt9140560','WandaVision','TV-PG',1, 2021, 'Blends the style of classic sitcoms with the MCU, in which Wanda Maximoff and Vision - two super-powered beings living their ideal suburban lives - begin to suspect that everything is not as it seems.')
 
 
 
@@ -330,6 +336,50 @@ VALUES('1.91', '28-JAN-93', NULL, NULL, 'Will Poulter');
 
 --Inserting actors from Infinity war
 
+--Actors from WandaVision/Loki
+INSERT INTO Actor
+VALUES('1.87', '09-FEB-81', 'Self-described blond curly "Gene Wilder" hair. His wide, warm smile. Expressive blue-green eyes. Deep smooth voice. Extremely polite, articulate and friendly personality', 'Hiddles', 'Tom Hiddleston');
+INSERT INTO Actor
+VALUES('1.70', NULL, NULL, NULL, 'Sophia Di Martino'); 
+INSERT INTO Actor
+VALUES('1.80', '18-NOV-68', 'Broken nose. His mop of blond hair. Soft mellow voice. Often works with Ben Stiller and his brother Luke Wilson. Often plays extremely mellow and friendly characters. Frequently cast by Wes Anderson. 
+Says "wow" at least once in almost every movie.', 'O', 'Owen Wilson');
+INSERT INTO Actor
+VALUES('1.74', '31-JUL-86', NULL, NULL, 'Wunmi Mosaku');
+INSERT INTO Actor
+VALUES('1.63', '21-APR-83', 'Mole on right side of upper lip.', NULL, 'Gugu Mbatha-Raw');
+INSERT INTO Actor
+VALUES('1.71', NULL, NULL, NULL, 'Eugene Cordero'); 
+INSERT INTO Actor
+VALUES(NULL, NULL, NULL, NULL, 'Neil Ellice'); 
+INSERT INTO Actor
+VALUES('1.59', '12-FEB-73', 'She usually provides the voices for young male characters in cartoons. The voice of Bubbles on The Powerpuff Girls (1998). The voice of Timmy in The Fairly OddParents (2001). Voice of Raven in Teen Titans (2003) and Twilight Sparkle in My Little Pony: Friendship Is Magic (2010). Blonde hair and buxom figure', NULL, 'Tara Strong'); 
+INSERT INTO Actor
+VALUES('1.65', '20-AUG-71', NULL, 'Jonathan', 'Ke Huy Quan'); 
+INSERT INTO Actor
+VALUES('1.75', NULL, NULL, NULL, 'Lauren Revard'); 
+
+INSERT INTO Actor
+VALUES('1.68', '16-FEB-89', 'Clear green eyes', 'Lizzie', 'Elizabeth Olsen');
+INSERT INTO Actor
+VALUES('1.91', '27-MAY-71', 'Scene-stealing supporting characters. Towering height and slender frame.', NULL, 'Paul Bettany');
+INSERT INTO Actor
+VALUES('1.65', '23-JUL-73', 'Over-the-top facial expressions. Wide smile.', NULL, 'Kathryn Hahn');
+INSERT INTO Actor
+VALUES('1.65', '22-SEP-87', NULL, NULL, 'Teyonah Parris');
+INSERT INTO Actor
+VALUES('1.87', '04-JAN-70', NULL, 'Josh', 'Josh Stamberg');
+INSERT INTO Actor
+VALUES(NULL, NULL, NULL, NULL, 'David A Payton');
+INSERT INTO Actor
+VALUES('1.78', NULL, NULL, NULL, 'David Lengel');
+INSERT INTO Actor
+VALUES(NULL, NULL, NULL, NULL, 'Amos Glick');
+INSERT INTO Actor
+VALUES('1.55', NULL, NULL, NULL, 'Selena Anduze');
+INSERT INTO Actor
+VALUES('1.75', NULL, NULL, NULL, 'Randall Park');
+
 
 
 
@@ -340,9 +390,21 @@ VALUES('1.91', '28-JAN-93', NULL, NULL, 'Will Poulter');
 INSERT INTO Director
 VALUES('1.83', '05-AUG-66', 'Heavy use of tongue-in-cheek dialogue. Unconventional characters. His spiky hair.', 'The Sneepers', 'James Gunn');
 
+--loki directors
+INSERT INTO Director
+VALUES(NULL, '28-OCT-87', NULL, NULL, 'Kate Herron');
+INSERT INTO Director
+VALUES('1.88', '09-JUN-83', NULL, NULL, 'Justin Benson');
+INSERT INTO Director
+VALUES(NULL, '17-DEC-87', NULL, 'NULL', 'Aaron Moorhead');
+INSERT INTO Director
+VALUES(NULL, NULL, NULL, 'NULL', 'Dan DeLeeuw');
+INSERT INTO Director
+VALUES(NULL, NULL, NULL, 'NULL', 'Kasra Farahani');
 
-
-
+--wandavision direct
+INSERT INTO Director
+VALUES(NULL, '08-AUG-75', NULL, NULL, 'Matt Shakman');
 
 
 --INSERT WRITERS HERE! (Height, Birthday, Trademark, Nickname, name)
@@ -364,35 +426,26 @@ VALUES('1.75', '09-OCT-49', NULL, NULL, 'Jim Starlin');
 INSERT INTO Writer
 VALUES('1.80', '28-DEC-22', 'His signature dark sunglasses. Cameos in the films based on his comics. Often referred to faithful fans as true believers.', 'The Man', 'Stan Lee');
 
+--wandavision write
+INSERT INTO Writer
+VALUES('1.93', NULL, NULL, NULL, 'Peter Cameron');
+INSERT INTO Writer
+VALUES(NULL, NULL, NULL, NULL, 'Mackenzie Dohr');
+INSERT INTO Writer
+VALUES(NULL, NULL, NULL, NULL, 'Laura Donney');
+INSERT INTO Writer
+VALUES(NULL, NULL, NULL, NULL, 'Bobak Esfarjani');
+INSERT INTO Writer
+VALUES(NULL, NULL, NULL, NULL, 'Megan McDonnell');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+--loki write
+INSERT INTO Writer
+VALUES(NULL, NULL, NULL, NULL, 'Eric Martin');
+INSERT INTO Writer
+VALUES('1.93', '23-APR-87', 'Wife works in pediatric plastic surgery.', NULL, 'Michael Waldron');
+INSERT INTO Writer
+VALUES(NULL,NULL,NULL, NULL, 'Bisha K. Ali');
+INSERT INTO Writer
+VALUES(NULL,NULL,NULL, NULL, 'Elissa Karasik');
+INSERT INTO Writer
+VALUES(NULL,NULL,NULL, NULL, 'Katharyn Blair');
